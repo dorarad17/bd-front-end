2061c28027dfc3593cb037d8d2ba4823a79f1c75
doraradosevic-bbc <dora.radosevic@bbc.co.uk>
2020-12-01 16:00:07 +0000

    basic form added

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,31 +1,63 @@
-import React, { useState } from "react";
+import React from "react";
+import { useForm } from "react-hook-form";
+import isEmail from "validator/lib/isEmail";
+
+const styles = {
+  container: {
+    width: "80%",
+    margin: "0 auto",
+  },
+  input: {
+    width: "100%",
+  },
+};
 
 function Signup() {
-	const [submitting, setSubmitting] = useState(false);
-	const handleSubmit = (event) => {
-		event.preventDefault();
-		setSubmitting(true);
+  const { register, handleSubmit, errors, formState } = useForm({
+    mode: "onBlur",
+  });
 
-		setTimeout(() => {
-			setSubmitting(false);
-		}, 3000);
-	};
+  function onSubmit(data) {
+    console.log(data); // { username: 'test', email: 'test', password: 'test' }
+  }
 
-	return (
-		<div className="wrapper">
-			<h1>Sign up here...</h1>
-			{submitting && <div>Submitting Form...</div>}
-			<form onSubmit={handleSubmit}>
-				<fieldset>
-					<label>
-						<p>First name</p>
-						<input name="name" />
-					</label>
-				</fieldset>
-				<button type="submit">Submit</button>
-			</form>
-		</div>
-	);
+  return (
+    <div style={styles.container}>
+      <h4>My Form</h4>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          name="firstname"
+          ref={register({
+            required: true,
+            pattern: /^[A-Za-z]+$/i,
+          })}
+          style={{ ...styles.input, borderColor: errors.firstname && "red" }}
+          placeholder="First Name"
+        />
+        <input
+          name="lastname"
+          ref={register({
+            required: true,
+            pattern: /^[A-Za-z]+$/i,
+          })}
+          style={{ ...styles.input, borderColor: errors.lastname && "red" }}
+          placeholder="Last Name"
+        />
+        <input
+          name="email"
+          ref={register({
+            required: true,
+            validate: (input) => isEmail(input), // returns true if valid
+          })}
+          style={{ ...styles.input, borderColor: errors.email && "red" }}
+          placeholder="Email"
+        />
+        <button type="submit" disabled={formState.isSubmitting}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Signup;
