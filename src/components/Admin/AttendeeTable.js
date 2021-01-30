b0914ee0e6c3dc6e9c0c9b72187f4e90bb74f506
doraradosevic-bbc <dora.radosevic@bbc.co.uk>
2021-01-30 16:53:32 +0000

    attendee list displaying

--- src/components/Admin/AttendeeTable.js
+++ src/components/Admin/AttendeeTable.js
@@ -2,28 +2,40 @@ import { React, useState, useEffect } from "react";
 import axios from "axios";
 
 function AttendeeTable() {
-  const [isLoading, setIsloading] = useState(true);
-  const [attendees, setAttendeeList] = useState(["None"]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [attendees, setAttendeeList] = useState({});
 
   useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = () => {
     axios
       .get("http://localhost:8081/attendees")
       .then((response) => {
         // handle success
         //console.log(response.data);
-        console.log(response);
-        setAttendeeList("test");
-        console.log(attendees);
-        setIsloading(false);
-        console.log(isLoading);
+        setAttendeeList(response.data);
+        setIsLoading(false);
       })
       .catch(function (error) {
         // handle error
         console.error(error);
       });
-  });
+  };
 
-  return <p>People coming:</p>;
+  return (
+    <div>
+      <p>People coming: </p>
+      <p>
+        <ul>
+          {attendees.map((item) => (
+            <li key={item.firstName}> {item.firstName}</li>
+          ))}
+        </ul>
+      </p>
+    </div>
+  );
 }
 
 export default AttendeeTable;
