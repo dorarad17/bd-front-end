882462a7730a13b5e14017640c89cba14ffd0a70
doraradosevic-bbc <dora.radosevic@bbc.co.uk>
2021-06-11 11:07:58 +0100

    initial tailwind config attempt

--- src/components/Admin/AttendingTable.js
+++ src/components/Admin/AttendingTable.js
@@ -1,44 +1,38 @@
 import { React } from "react";
-import Button from "react-bootstrap/Button";
 
 function AttendingTable({ attendees }) {
   return (
-    <section className="App h-screen w-full flex justify-center items-center bg-green-500">
-      <div className="w-full max-w-md bg-gray-800">
-        <form action="" className=" bg-white shadow-md rounded px-8 py-8 pt-8">
-          <div className="px-4 pb-4">
-            <label htmlFor="email" className="text-sm block font-bold  pb-2">
-              EMAIL ADDRESS
-            </label>
-            <input
-              type="email"
-              name="email"
-              id=""
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-300 "
-              placeholder="Johnbull@example.com"
-            />
-          </div>
-          <div className="px-4 pb-4">
-            <label htmlFor="password" className="text-sm block font-bold pb-2">
-              PASSWORD
-            </label>
-            <input
-              type="password"
-              name="email"
-              id=""
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-300"
-              placeholder="Enter your password"
-            />
-          </div>
-          <div>
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="button"
-            >
-              Sign In
-            </button>
-          </div>
-        </form>
+    <section className="App h-screen w-full flex justify-center items-center bg-white">
+      <div className="w-full max-w-md bg-gray-300">
+        <table class="table-auto">
+          <thead>
+            <tr>
+              <th class="w-1/2 ...">Title</th>
+              <th class="w-1/4 ...">Author</th>
+              <th class="w-1/4 ...">Views</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Intro to CSS</td>
+              <td>Adam</td>
+              <td>858</td>
+            </tr>
+            <tr class="bg-blue-200">
+              <td>
+                A Long and Winding Tour of the History of UI Frameworks and
+                Tools and the Impact on Design
+              </td>
+              <td>Adam</td>
+              <td>112</td>
+            </tr>
+            <tr>
+              <td>Intro to JavaScript</td>
+              <td>Chris</td>
+              <td>1,280</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </section>
   );
